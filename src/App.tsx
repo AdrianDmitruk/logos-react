0cda042c3a4846e760c6150bf0a7bd7a95171f7c
AdrianDmitruk <adriandmitruk@gmail.com>
2022-10-19 16:08:57 +0300

    first test

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
 import './scss/App.scss'
 import React, { Suspense } from 'react'
-import { Routes, Route, } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom'
 
-import Home from './pages/Home';
-import MainLayout from './layouts/MainLayout';
+import Home from './pages/Home'
+import MainLayout from './layouts/MainLayout'
 
-const Cart = React.lazy(() => import('./pages/Cart'));
-const FoolFood = React.lazy(() => import('./pages/FoolFood'));
-const NotFound = React.lazy(() => import('./pages/NotFound'));
+const Cart = React.lazy(() => import('./pages/Cart'))
+const FoolFood = React.lazy(() => import('./pages/FoolFood'))
+const NotFound = React.lazy(() => import('./pages/NotFound'))
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<MainLayout />}>
-        <Route path="" element={<Home />} />
-        <Route path="cart" element={
-          <Suspense fallback={<div>Загрузка...</div>}>
-            <Cart />
-          </Suspense>
-        } />
-        <Route path="food/:id" element={
-          <Suspense fallback={<div>Загрузка...</div>}>
-            <FoolFood />
-          </Suspense>} />
-        <Route path="*" element={
-          <Suspense fallback={<div>Загрузка...</div>}>
-            <NotFound />
-          </Suspense>} />
+      <Route path="/logos-react/" element={<MainLayout />}>
+        <Route path="/logos-react/" element={<Home />} />
+        <Route
+          path="/logos-react/cart"
+          element={
+            <Suspense fallback={<div>Загрузка...</div>}>
+              <Cart />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/logos-react/food/:id"
+          element={
+            <Suspense fallback={<div>Загрузка...</div>}>
+              <FoolFood />
+            </Suspense>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <Suspense fallback={<div>Загрузка...</div>}>
+              <NotFound />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
-  );
+  )
 }
 
-export default App;
+export default App
